c7f137a1abe2a1e34128e271b21c88e4f0e5bde5
Joe Wicentowski <joewiz@gmail.com>
2015-10-02 15:17:17 -0400

    run bower install

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,7 +16,7 @@ cd repos
 for ((i=0;i<${#repos[@]};++i));
 do
   if cd ${repos[i]};
-    then echo "Building" ${repos[i]}; /usr/local/bin/ant; cp build/* ../../xars/; cd ..;
+    then echo "Building" ${repos[i]}; /usr/local/bin/bower install; /usr/local/bin/ant; cp build/* ../../xars/; cd ..;
     else echo "Not found:" ${repos[i]}". Please run pull.sh";
   fi
 done
