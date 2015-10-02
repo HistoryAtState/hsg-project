048ccc3f4f41c82865ad04219515d13b2f893e7f
Joe Wicentowski <joewiz@gmail.com>
2015-10-02 11:41:42 -0400

    make xars directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,13 +10,13 @@ repos=(
   "gsh"
 )
 
+mkdir xars
 cd repos 
 
 for ((i=0;i<${#repos[@]};++i));
 do
-  echo "building" ${repos[i]}
   if cd ${repos[i]};
-    then echo "Pulling" ${repos[i]}; /usr/local/bin/ant; cp build/* ../../xars/; cd ..;
+    then echo "Building" ${repos[i]}; /usr/local/bin/ant; cp build/* ../../xars/; cd ..;
     else echo "Not found:" ${repos[i]}". Please run pull.sh";
   fi
 done
