d9772870619be8e038d63622e8f9630eaea41f67
tasmo <tasmo@tasmo.de>
2016-07-11 19:27:27 +0200

    added auto update script for Let's Encrypt

---
+++ deploy/1861/letsencrypt/letsencrypt-renew.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+config_file='/letsencrypt/cli-1861.history.state.gov.ini'
+
+le_path='/opt/letsencrypt'
+exp_limit=30;
+
+if [ ! -f $config_file ]; then
+  echo "[ERROR] config file does not exist: $config_file"
+  exit 1;
+fi
+
+domain=`grep "^\s*domains" $config_file | sed "s/^\s*domains\s*=\s*//" | sed 's/(\s*)\|,.*$//'`
+cert_file="/etc/letsencrypt/live/$domain/fullchain.pem"
+
+if [ ! -f $cert_file ]; then
+  echo "[ERROR] certificate file not found for domain $domain."
+fi
+
+exp=$(date -d "`openssl x509 -in $cert_file -text -noout|grep "Not After"|cut -c 25-`" +%s)
+datenow=$(date -d "now" +%s)
+days_exp=$(echo \( $exp - $datenow \) / 86400 |bc)
+
+echo "Checking expiration date for $domain..."
+
+if [ "$days_exp" -gt "$exp_limit" ] ; then
+  echo "The certificate is up to date, no need for renewal ($days_exp days left)."
+  exit 0;
+else
+  echo "The certificate for $domain is about to expire soon."
+  echo "Stopping nginx"
+    service nginx stop
+  echo "Starting renewal script..."
+    $le_path/letsencrypt-auto certonly --agree-tos --renew-by-default --config $config_file --debug
+  echo "Reloading nginx"
+    nginx -t && service nginx start
+  echo "Renewal process finished for domain $domain"
+  exit 0;
+fi
---
+++ deploy/automate_letsencrypt_ca.md
@@ -0,0 +1,28 @@
+## Let’s Encrypt
+
+[Let’s Encrypt] provides free SSL certificates with its own Certificate Authority.
+
+The certificates are valid for three month and have to be renewed in between this period.
+
+To get a certificate it is necessary to install the [Let’s Encrypt] client [certbot]; which is done in the directory `/opt/letsencrypt/`.
+
+## The Renewal Script
+
+The script (`/letsencrypt/letsencrypt-renew.sh`) will be executed with a crontab task:
+
+~~~shell
+sudo crontab -e
+~~~
+
+with content
+
+~~~text
+30 2 * * 1 /letsencrypt/letsencrypt-renew.sh >> /letsencrypt/letsencrypt-renewal.log
+~~~
+
+If the validation period comes to its end the script will start the renew process. Through this __the nginx web server will be stopped__; the [certbot] client generates a new certificate and start its own web server to get verified by the [Let’s Encrypt] CA. Afterwards nginx will be started again.
+
+The result is logged to `/letsencrypt/letsencrypt-renewal.log`.
+
+[Let’s Encrypt]: https://letsencrypt.org/
+[certbot]: https://github.com/certbot/certbot
