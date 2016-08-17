4866570f883f52ba297f09b9b03e8594f37d1ba5
tasmo <tasmo@tasmo.de>
2016-08-17 18:11:19 +0200

    pull instead of fetch

--- deploy/1861/HOME/nginx-renew.sh
+++ deploy/1861/HOME/nginx-renew.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/hsg-project && git fetch
+cd /home/ec2-user/hsg-project && git pull
 /usr/bin/rsync -a /home/ec2-user/hsg-project/deploy/1861/etc/nginx/* /etc/nginx && chown -R root:root /etc/nginx/
 /usr/sbin/nginx -t && /sbin/service nginx reload
 echo "nginx config updated successfully"
