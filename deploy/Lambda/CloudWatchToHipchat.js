9f28702cf71f3d782cd2f04a530ad5364e91f63a
Joe Wicentowski <joewiz@gmail.com>
2016-10-27 09:49:52 -0400

    AWS Lambda script for sending Cloudwatch SNS notifications to our HipChat room

---
+++ deploy/Lambda/CloudWatchToHipchat.js
@@ -0,0 +1,67 @@
+// Adapted from https://github.com/rvrangel/cloudwatch-hipchat/blob/master/CloudWatchToHipchat.js
+// See See http://blog.renanrangel.com/2015/07/24/sending-aws-cloudwatch-alarms-to-a-hipchat-room/
+
+var http = require('https');
+
+// update with your hipchat data
+var hipchatToken = '';
+var hipchatRoom = '';
+
+exports.handler = function(event, context) {
+    
+    // console.log('event: ' + JSON.stringify(event));
+    
+    var message = event.Records[0].Sns.Message;
+
+    // CloudWatch messages are escaped JSON!
+
+    var alarm = JSON.parse(message)
+
+    var snsMessage = '(' + alarm.AlarmName + '){' + alarm.NewStateValue + '} ' + alarm.NewStateReason;
+    
+    var hipchatColor = alarm.NewStateValue === 'ALARM' ? 'red' : 'green';
+    
+    var hipchatMessage = JSON.stringify({
+        color: hipchatColor,
+        message: snsMessage,
+        notify: true,
+        message_format: 'text'
+    });
+    
+    console.log('hipchat message: ' + hipchatMessage);
+    
+    var httpOptions = {
+        host: 'api.hipchat.com',
+        port: 443,
+        method: 'POST',
+        path: '/v2/room/' + hipchatRoom + '/notification?auth_token=' + hipchatToken,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    
+    var req = http.request(httpOptions, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+        res.on('end', function () {
+            console.log(res.statusCode);
+            if (res.statusCode === 204) {
+                console.log('success');
+                context.succeed('message delivered to hipchat');
+            } else {
+                console.log('failed');
+                context.fail('hipchat API returned an error');
+            }
+        });
+    });
+
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+        context.fail('failed to deliver message to hipchat');
+    });
+
+    req.write(hipchatMessage);
+    req.end();
+};
