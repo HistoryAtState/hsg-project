75c1ee208f6d2b78b8100334733d077c1f38694e
Thomas Friese <tasmo@tasmo.de>
2018-03-12 11:13:22 +0100

    adds config path to Let's Encrypt renwal
    change into configuration directory

--- deploy/1861/HOME/letsencrypt/letsencrypt-new.sh
+++ deploy/1861/HOME/letsencrypt/letsencrypt-new.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # file with configurable options
+config_path="/root/letsencrypt/"
 config_file="/root/letsencrypt/cli-1861.history.state.gov.ini"
 
 # where to write log output
@@ -12,6 +13,7 @@ le_path="/opt/letsencrypt"
 # start renewal
 echo "Starting renewal script..." | tee -a "$log_file"
 # the renwal process itself
+cd "$config_path" || return
 $le_path/certbot-auto certonly --config "$config_file" --agree-tos --debug
 # restarting the web server
 echo "Reloading nginx" | tee -a "$log_file"
