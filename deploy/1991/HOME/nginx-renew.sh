469747dc6515ad11b3ed8819be2f8afe6dcee766
Joe Wicentowski <joewiz@gmail.com>
2024-04-01 12:49:47 -0400

    Remove deploy directory
    Server configuration is now handled by other facilities

--- deploy/1991/HOME/nginx-renew.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ec2-user/hsg-project && git pull
-/usr/bin/rsync -a /home/ec2-user/hsg-project/deploy/1991/etc/nginx/* /etc/nginx && sudo chown -R root:root /etc/nginx/
-sudo /usr/sbin/nginx -t && sudo /sbin/service nginx reload
-echo "nginx config updated successfully"
